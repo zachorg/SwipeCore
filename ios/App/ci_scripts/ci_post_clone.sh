a2abeab3dda86b6da37f0b4cc2f06fe53aba9994
Vrij Patel <vrijpatel123@icloud.com>
2025-08-22 17:25:37 -0400

    test 2

--- ios/App/ci_scripts/ci_post_clone.sh
+++ ios/App/ci_scripts/ci_post_clone.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+# Use nvm to install specific Node.js version (if needed)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install 18
+nvm use 18
+
 # Install dependencies
-# XCode Cloud is literally broken for 2 months now - https://developer.apple.com/forums/thread/738136?answerId=774510022#774510022
 npm config set maxsockets 3
 npm ci
-# or `pnpm install --frozen-lockfile` or `yarn install --frozen-lockfile` or bun install
 npm run build
-# or npm run build
-npm run sync:ios
+npx cap sync ios --deployment
