6d3cfdfb6a49fc15d7b0e25cb4fa8a1b31cfe822
Vrij Patel <vrijpatel123@icloud.com>
2025-08-22 17:23:25 -0400

    test

--- ios/App/ci_scripts/ci_post_clone.sh
+++ ios/App/ci_scripts/ci_post_clone.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-set -x
-
-export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
-# Install CocoaPods
-echo "📦 Install CocoaPods"
-brew install cocoapods
-brew install node@18
-brew link node@18
-
 # Install dependencies
 # XCode Cloud is literally broken for 2 months now - https://developer.apple.com/forums/thread/738136?answerId=774510022#774510022
 npm config set maxsockets 3
